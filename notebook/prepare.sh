7c6bdd58fcc796ed617f9dbcdc6485975d8e40ad
Matthew Rocklin <mrocklin@gmail.com>
2017-12-03 08:51:49 -0500

    Copy prepare.sh from base/ to notebook/

---
+++ notebook/prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -x
+
+if [ -e "/opt/app/environment.yml" ]; then
+    echo "environment.yml found. Installing packages"
+    /opt/conda/bin/conda env update -n dask -f /opt/app/environment.yml
+else
+    echo "no environment.yml"
+fi
+
+if [ "$EXTRA_CONDA_PACKAGES" ]; then
+    echo "EXTRA_CONDA_PACKAGES environment variable found.  Installing."
+    /opt/conda/bin/conda install -n dask $EXTRA_CONDA_PACKAGES
+fi
+
+if [ "$EXTRA_PIP_PACKAGES" ]; then
+    echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
+    /opt/conda/envs/dask/bin/pip install $EXTRA_PIP_PACKAGES
+fi
+
+# Run extra commands
+$@
