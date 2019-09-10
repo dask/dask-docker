b21005eb07a4d673597d805acb07ad908d8a30f1
Moe Faraji <faraji66@gmail.com>
2019-09-10 19:42:50 -0400

    Upgrade dask Install boogiesvc

--- base/prepare.sh
+++ base/prepare.sh
@@ -2,29 +2,32 @@
 
 set -x
 
-# # We start by adding extra apt packages, since pip modules may required library
-# if [ "$EXTRA_APT_PACKAGES" ]; then
-#     echo "EXTRA_APT_PACKAGES environment variable found.  Installing."
-#     apt update -y
-#     apt install -y $EXTRA_APT_PACKAGES
-# fi
+# We start by adding extra apt packages, since pip modules may required library
+if [ "$EXTRA_APT_PACKAGES" ]; then
+    echo "EXTRA_APT_PACKAGES environment variable found.  Installing."
+    apt update -y
+    apt install -y $EXTRA_APT_PACKAGES
+fi
 
-# if [ -e "/opt/app/environment.yml" ]; then
-#     echo "environment.yml found. Installing packages"
-#     /opt/conda/bin/conda env update -f /opt/app/environment.yml
-# else
-#     echo "no environment.yml"
-# fi
+if [ -e "/opt/app/environment.yml" ]; then
+    echo "environment.yml found. Installing packages"
+    /opt/conda/bin/conda env update -f /opt/app/environment.yml
+else
+    echo "no environment.yml"
+fi
 
-# if [ "$EXTRA_CONDA_PACKAGES" ]; then
-#     echo "EXTRA_CONDA_PACKAGES environment variable found.  Installing."
-#     /opt/conda/bin/conda install -y $EXTRA_CONDA_PACKAGES
-# fi
+if [ "$EXTRA_CONDA_PACKAGES" ]; then
+    echo "EXTRA_CONDA_PACKAGES environment variable found.  Installing."
+    /opt/conda/bin/conda install -y $EXTRA_CONDA_PACKAGES
+fi
 
-# if [ "$EXTRA_PIP_PACKAGES" ]; then
-#     echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
-#     /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
-# fi
+if [ "$EXTRA_PIP_PACKAGES" ]; then
+    echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
+    /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
+fi
+
+BOOGIE_VERSIONS=$(cat ./BOOGIE_VERSION)
+pip install boogiesvc==${BOOGIE_VERSIONS} --extra-index-url https://pypi.spraoi-sandbox.dev/
 
 # Run extra commands
 exec "$@"
