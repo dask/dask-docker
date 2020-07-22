a17f32316da50346ca8158152ed872fc622b7cb4
Andrew McNichols <amcnicho@nrao.edu>
2020-07-22 10:04:22 -0400

    Run only apt commands as root in notebook/prepare.sh

--- notebook/prepare.sh
+++ notebook/prepare.sh
@@ -9,6 +9,9 @@ if [ "$EXTRA_APT_PACKAGES" ]; then
     apt install -y $EXTRA_APT_PACKAGES
 fi
 
+# Switch user so only ^^^ is executed as root
+su - $NB_USER
+
 if [ -e "/opt/app/environment.yml" ]; then
     echo "environment.yml found. Installing packages"
     /opt/conda/bin/conda env update -f /opt/app/environment.yml
@@ -26,8 +29,7 @@ if [ "$EXTRA_PIP_PACKAGES" ]; then
     /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
 fi
 
-# Start jupyter here instead of Dockerfile so that ^^^ is executed as root
-su - $NB_USER && start.sh jupyter lab
+start.sh jupyter lab
 
 # Run extra commands
 exec "$@"
