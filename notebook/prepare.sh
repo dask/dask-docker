b92cc91fd3ae3f771edd841ec865ab740af05945
Andrew Thomas <andrew.mcnichols@gmail.com>
2020-08-11 05:15:24 -0400

    Fix EXTRA_APT_PACKAGES permission errors in notebook image (#110)
    * Update Dockerfile
    
    Run `/usr/bin/prepare.sh` as root so EXTRA_APT_PACKAGES commands execute without permission error
    
    * Move jupyter lab start command from Dockerfile to prepare.sh
    
    * Run only apt commands as root in notebook/prepare.sh

--- notebook/prepare.sh
+++ notebook/prepare.sh
@@ -9,6 +9,9 @@ if [ "$EXTRA_APT_PACKAGES" ]; then
     apt install -y $EXTRA_APT_PACKAGES
 fi
 
+# Switch user so only ^^^ is executed as root
+su - $NB_USER
+
 if [ -e "/opt/app/environment.yml" ]; then
     echo "environment.yml found. Installing packages"
     /opt/conda/bin/conda env update -f /opt/app/environment.yml
@@ -25,6 +28,8 @@ if [ "$EXTRA_PIP_PACKAGES" ]; then
     echo "EXTRA_PIP_PACKAGES environment variable found.  Installing".
     /opt/conda/bin/pip install $EXTRA_PIP_PACKAGES
 fi
+
+start.sh jupyter lab
 
 # Run extra commands
 exec "$@"
