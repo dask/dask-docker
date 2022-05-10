836784ae2a49145cb5bb2bc00014d12c17d609e7
Charles Blackmon-Luca <20627856+charlesbluca@users.noreply.github.com>
2022-05-10 11:31:53 -0700

    Fix typo in prepare scripts

--- notebook/prepare.sh
+++ notebook/prepare.sh
@@ -19,7 +19,7 @@ fi
 if [ "$NIGHTLY_DASK" == "true" ]; then
     echo "NIGHTLY_DASK enabled. Installing latest Dask nightly conda packages."
     $CONDA_BIN update -c dask/label/dev -y dask
-if
+fi
 
 if [ -e "/opt/app/environment.yml" ]; then
     echo "environment.yml found. Installing packages"
